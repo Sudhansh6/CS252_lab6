c8587a5834a2248ece7c6a137207190043fccd24
sudhansh6 <sudhansh6@gmail.com>
2021-05-06 14:21:45 +0530

    All files are working

---
+++ client.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SERVER_PORT 9000
+#define SIZE 1024
+// The code from the reference links mentioned in the document is used as a template.
+// This sends the textfile
+// Usage: ./client localhost cubic or ./client 127.0.0.1 reno
+void send_file(FILE *fp, int s){
+  int n;
+  char data[SIZE] = {0};
+
+  while(fgets(data, SIZE, fp) != NULL) {
+    if (send(s, data, sizeof(data), 0) == -1) {
+      perror("Error in sending file.");
+      exit(1);
+    }
+    bzero(data, SIZE);
+  }
+}
+
+int main(int argc, char * argv[])
+{
+  FILE *fp;
+  char *filename = "input.txt";
+  struct hostent *hp;
+  struct sockaddr_in sin;
+  char *host, *TCP_variant;
+  int s;
+  int len;
+
+  if (argc==3) {
+    host = argv[1];
+    TCP_variant = argv[2];
+  }
+  else {
+    fprintf(stderr, "usage: simplex-talk host TCP_variant\n");
+    exit(1);
+  }
+
+  /* translate host name into peer's IP address */
+  hp = gethostbyname(host);
+  if (!hp) {
+    fprintf(stderr, "simplex-talk: unknown host: %s\n", host);
+    exit(1);
+  }
+  else
+    printf("Host detected.\n");
+
+  /* build address data structure */
+  bzero((char *)&sin, sizeof(sin));
+  sin.sin_family = AF_INET;
+  bcopy(hp->h_addr, (char *)&sin.sin_addr, hp->h_length);
+  sin.sin_port = htons(SERVER_PORT);
+
+  /* active open */
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  else
+    printf("Socket created.\n");
+
+  // Set TCP protocol
+  len = strlen(TCP_variant);
+  if (setsockopt(s, IPPROTO_TCP, TCP_CONGESTION, TCP_variant, len) != 0)
+  {
+      perror("setsockopt");
+      return -1;
+  }
+  len = sizeof(TCP_variant);
+  if (getsockopt(s, IPPROTO_TCP, TCP_CONGESTION, TCP_variant, &len) != 0)
+  {
+      perror("getsockopt");
+      return -1;
+  }
+  printf("Client is using TCP %s.\n", TCP_variant);
+
+  if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+  {
+    perror("simplex-talk: connect");
+    close(s);
+    exit(1);
+  }
+  else
+    printf("Connection successful.\n");
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    perror("Error in reading file.");
+    exit(1);
+  }
+
+  send_file(fp, s);
+  printf("File data sent successfully.\n");
+
+  printf("Closing the connection.\n");
+  close(s);
+
+  return 0;
+
+}
---
+++ server.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SERVER_PORT  9000
+#define MAX_PENDING  5
+#define MAX_LINE     256
+#define SIZE         1024
+// The code from the reference links mentioned in the document is used as a template.
+// This receives the textfile
+// Usage: ./server reno
+void write_file(int s)
+{
+  int n;
+  FILE *fp;
+  char *filename = "output.txt";
+  char buffer[SIZE];
+
+  fp = fopen(filename, "w");
+  while (1) {
+    n = recv(s, buffer, SIZE, 0);
+    if (n <= 0){
+      break;
+      return;
+    }
+    fprintf(fp, "%s", buffer);
+    bzero(buffer, SIZE);
+  }
+  return;
+}
+
+int main(int argc, char * argv[])
+{
+  struct sockaddr_in sin; 
+  char buf[MAX_LINE];
+  char *TCP_variant;
+  int buf_len, addr_len;
+  int s, new_s, len;
+  struct sockaddr_in server_addr, new_addr;
+
+  if (argc==2) {
+    TCP_variant = argv[1];
+  }
+  else {
+    fprintf(stderr, "usage: simplex-talk TCP_variant\n");
+    exit(1);
+  }
+
+  /* build address data structure */
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(SERVER_PORT);
+
+  /* setup passive open */
+  if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("simplex-talk: socket");
+    exit(1);
+  }
+  else
+    printf("Socket created.\n");
+
+  len = strlen(TCP_variant);
+  if (setsockopt(s, IPPROTO_TCP, TCP_CONGESTION, TCP_variant, len) != 0)
+  {
+      perror("setsockopt");
+      return -1;
+  }
+  len = sizeof(TCP_variant);
+  if (getsockopt(s, IPPROTO_TCP, TCP_CONGESTION, TCP_variant, &len) != 0)
+  {
+      perror("getsockopt");
+      return -1;
+  }
+  printf("Server is using TCP %s.\n", TCP_variant);
+
+  if ((bind(s, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0) {
+    perror("simplex-talk: bind");
+    exit(1);
+  }
+  else
+    printf("Socket bind success.\n");
+  
+  if(listen(s, 10) == 0)
+    printf("Listening....\n");
+  else
+  { 
+    perror("Error in listening");
+    exit(1);
+  }
+
+  socklen_t addr_size = sizeof(new_addr);
+  new_s = accept(s, (struct sockaddr*)&new_addr, &addr_size);
+  write_file(new_s);
+  printf("Data written in the file successfully.\n");
+
+  return 0;
+}
