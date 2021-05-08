aa61387d675a8cbc5f0b8aad4b557f1220172bd0
sudhansh6 <sudhansh6@gmail.com>
2021-05-08 21:47:53 +0530

    will fix bash

--- plotter.py
+++ plotter.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
+from math import sqrt
 
 counter = 0
 if(len(sys.argv) != 3):
@@ -12,33 +13,36 @@ t2 = sys.argv[2]
 tcp1 = tcp2 = tcp1tp_mean = tcp1tp_std = tcp2tp_mean = tcp2tp_std = []
 f2 = open("results.txt", 'w')
 with open('textfile.txt','r') as f:
-    for line in f:
-        f2.write(line)
-        if counter == 0:
-            f2.write("\n")
-        if(counter%5 == 0):
-            tcp1=[]
-            tcp2=[]
-        elif(counter%5 == 2):
-            tcp1 = line[0:-2].split(" ")
-            tcp1 = [float(i) for i in tcp1]
-            tcp1_mean = np.mean(tcp1)
-            f2.write("Throughput mean  = {} bits/sec\n".format(tcp1_mean))
-            tcp1_std = np.std(tcp1)
-            f2.write("Throughput standard deviation  = {} bits/sec\n\n".format(tcp1_std))
-        elif(counter%5 == 4):
-            tcp2 = line[0:-2].split(" ")
-            tcp2 = [float(i) for i in tcp2]
-            tcp2_mean = np.mean(tcp2)
-            f2.write("Throughput mean  = {} bits/sec\n".format(tcp2_mean))
-            tcp2_std = np.std(tcp2)
-            f2.write("Throughput standard deviation  = {} bits/sec\n\n".format(tcp2_std))
-        counter+=1
-        if(counter%5 == 0 and counter>0):
-            tcp1tp_mean.append(tcp1_mean)
-            tcp1tp_std.append(tcp1_std)
-            tcp2tp_mean.append(tcp2_mean)
-            tcp2tp_std.append(tcp2_std)
+	for line in f:
+		f2.write(line)
+		if counter == 0:
+			f2.write("\n")
+		if(counter%5 == 0):
+			tcp1=[]
+			tcp2=[]
+		elif(counter%5 == 2):
+			tcp1 = line[0:-2].split(" ")
+			tcp1 = [float(i) for i in tcp1]
+			tcp1_mean = np.mean(tcp1)
+			f2.write("Throughput mean  = {} bits/sec\n".format(tcp1_mean))
+			tcp1_std = np.std(tcp1)
+			f2.write("Throughput standard deviation  = {} bits/sec\n\n".format(tcp1_std))
+		elif(counter%5 == 4):
+			tcp2 = line[0:-2].split(" ")
+			tcp2 = [float(i) for i in tcp2]
+			tcp2_mean = np.mean(tcp2)
+			f2.write("Throughput mean  = {} bits/sec\n".format(tcp2_mean))
+			tcp2_std = np.std(tcp2)
+			f2.write("Throughput standard deviation  = {} bits/sec\n\n".format(tcp2_std))
+		counter+=1
+		if(counter%5 == 0 and counter>0):
+			tcp1tp_mean.append(tcp1_mean)
+			tcp1tp_std.append(tcp1_std)
+			tcp2tp_mean.append(tcp2_mean)
+			tcp2tp_std.append(tcp2_std)
+
+tcp1tp_std = list(map(lambda c: c/2.718, tcp1tp_std)) # 1.645*tcp1tp_std/sqrt(20)
+tcp2tp_std = list(map(lambda c: c/2.718, tcp2tp_std)) # 1.645*tcp2tp_std/sqrt(20)
 
 #index 0 - loss0.1 delay10
 #index 1 - loss0.5 delay10
@@ -52,36 +56,45 @@ with open('textfile.txt','r') as f:
 delay = np.array([10,50,100])
 loss = np.array([0.1,0.5,1])
 for i in range(3):
-    ytcp2 = np.array([tcp2tp_mean[i],tcp2tp_mean[i+3],tcp2tp_mean[i+6]])
-    ytcp1 = np.array([tcp1tp_mean[i],tcp1tp_mean[i+3],tcp1tp_mean[i+6]])
-    tcp1_std = np.array([tcp1tp_std[i],tcp1tp_std[i+3],tcp1tp_std[i+6]])
-    tcp2_std = np.array([tcp2tp_std[i],tcp2tp_std[i+3],tcp2tp_std[i+6]])
-    # citcp1 = 0.9*np.std(ytcp1)/np.mean(ytcp1)
-    # citcp2 = 0.9*np.std(ytcp2)/np.mean(ytcp2)
-    plt.errorbar(delay, ytcp1, yerr = 1.645*tcp1_std, label=t1, marker='.', alpha = 0.5)
-    # plt.fill_between(delay,ytcp1-citcp1,ytcp1+citcp1,color='blue',alpha=0.1)
-    plt.errorbar(delay,ytcp2, yerr = 1.645*tcp2_std, label=t2, marker='.',  alpha = 0.5)
-    # plt.fill_between(delay,ytcp2-citcp2,ytcp2+citcp2,color='blue',alpha=0.1)
-    plt.title('Mean throughput vs Delay for Loss=%s'%loss[i])
-    plt.xlabel('Delay')
-    plt.ylabel('Throughput')
-    plt.legend()
-    plt.savefig('Figures/Plot-%s.png'%(i+1))
-    plt.figure()
+	plt.title('Mean throughput vs Delay for Loss=%s'%loss[i])
+	plt.xlabel('Delay')
+	plt.ylabel('Throughput')
+	plt.grid()
+
+	ytcp2 = np.array([tcp2tp_mean[i],tcp2tp_mean[i+3],tcp2tp_mean[i+6]])
+	ytcp1 = np.array([tcp1tp_mean[i],tcp1tp_mean[i+3],tcp1tp_mean[i+6]])
+	tcp1_std = np.array([tcp1tp_std[i],tcp1tp_std[i+3],tcp1tp_std[i+6]])
+	tcp2_std = np.array([tcp2tp_std[i],tcp2tp_std[i+3],tcp2tp_std[i+6]])
+
+	plt.errorbar(delay, ytcp1, yerr = tcp1_std, label=t1, marker='.', alpha = 0.5)
+	plt.errorbar(delay, ytcp2, yerr = tcp2_std, label=t2, marker='.',  alpha = 0.5)
+	plt.plot(delay, ytcp1, color="red")
+	plt.fill_between(delay, ytcp1 - tcp1_std, ytcp1 + tcp1_std, color='red', alpha=0.1)
+	plt.plot(delay, ytcp2, color="blue")
+	plt.fill_between(delay, ytcp2 - tcp2_std, ytcp2 + tcp2_std, color='blue', alpha=0.1)
+	plt.legend()
+	
+	plt.savefig('Figures/Plot-%s.png'%(i+1))
+	plt.figure()
+
 for i in range(3):
-    ytcp2 = np.array([tcp2tp_mean[3*i],tcp2tp_mean[3*i+1],tcp2tp_mean[3*i+2]])
-    ytcp1 = np.array([tcp1tp_mean[3*i],tcp1tp_mean[3*i+1],tcp1tp_mean[3*i+2]])
-    tcp1_std = np.array([tcp1tp_std[3*i],tcp1tp_std[3*i+1],tcp1tp_std[3*i+2]])
-    tcp2_std = np.array([tcp2tp_std[3*i],tcp2tp_std[3*i+1],tcp2tp_std[3*i+2]])
-    # citcp1 = 0.9*np.std(ytcp1)/np.mean(ytcp1)
-    # citcp2 = 0.9*np.std(ytcp2)/np.mean(ytcp2)
-    plt.errorbar(loss,ytcp1, yerr = 1.645*tcp1_std,label=t1,marker='.', alpha = 0.5)
-    # plt.fill_between(loss,ytcp1-citcp1,ytcp1+citcp1,color='blue',alpha=0.1)
-    plt.errorbar(loss,ytcp2, yerr = 1.645*tcp2_std,label=t2,marker='.', alpha = 0.5)
-    # plt.fill_between(loss,ytcp2-citcp2,ytcp2+citcp2,color='blue',alpha=0.1)
-    plt.title('Mean throughput vs Loss for Delay=%sms'%delay[i])
-    plt.xlabel('Loss')
-    plt.ylabel('Throughput')
-    plt.legend()
-    plt.savefig('Figures/Plot-%s.png'%(i+4))
-    plt.figure()
+	plt.title('Mean throughput vs Loss for Delay=%sms'%delay[i])
+	plt.xlabel('Loss')
+	plt.ylabel('Throughput')
+	plt.grid()
+
+	ytcp2 = np.array([tcp2tp_mean[3*i],tcp2tp_mean[3*i+1],tcp2tp_mean[3*i+2]])
+	ytcp1 = np.array([tcp1tp_mean[3*i],tcp1tp_mean[3*i+1],tcp1tp_mean[3*i+2]])
+	tcp1_std = np.array([tcp1tp_std[3*i],tcp1tp_std[3*i+1],tcp1tp_std[3*i+2]])
+	tcp2_std = np.array([tcp2tp_std[3*i],tcp2tp_std[3*i+1],tcp2tp_std[3*i+2]])
+
+	plt.errorbar(delay, ytcp1, yerr = tcp1_std, label=t1, marker='.', alpha = 0.5)
+	plt.errorbar(delay, ytcp2, yerr = tcp2_std, label=t2, marker='.',  alpha = 0.5)
+	plt.plot(delay, ytcp1, color="red")
+	plt.fill_between(delay, ytcp1 - tcp1_std, ytcp1 + tcp1_std, color='red', alpha=0.1)
+	plt.plot(delay, ytcp2, color="blue")
+	plt.fill_between(delay, ytcp2 - tcp2_std, ytcp2 + tcp2_std, color='blue', alpha=0.1)
+	plt.legend()
+	
+	plt.savefig('Figures/Plot-%s.png'%(i+4))
+	plt.figure()
